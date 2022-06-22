3f24d1b7eaa225d36402add5ac1a611b69f4fa61
sh2002vk <sh2002vk@gmail.com>
2022-06-21 23:54:15 -0400

    started storing data

--- risk_arbitrage/data_ingestion/newsapi.py
+++ risk_arbitrage/data_ingestion/newsapi.py
@@ -3,15 +3,25 @@ import os
 import requests
 import json
 from api_list import api_urls
+from datetime import datetime
+
+
+def convertDates(date: str):
+    epoch = datetime(1970, 1, 1)
+    creation_epoch = 0
+
+    try:
+        creation_epoch = (datetime.strptime(date, "%Y-%m-%dT%H:%M:%SZ") - epoch).total_seconds()
+    except Exception as e:
+        print(e)
+
+    return creation_epoch
+
 
 # API info
 api_key = os.getenv("newsapiKey")
 api_endpoint = api_urls["newsapi"]
-q = "merge OR merger OR buyout OR acquisition"
-
-# Data store info
-path = "/Users/shubh/Desktop/Ruby/R & D/ruby_reasearch/risk_arbitrage/data_store/newsapi_store.json"
-
+q = "merger"  # Keywords the api is looking for
 params = {
     "apiKey": api_key,
     "q": q,
@@ -19,13 +29,33 @@ params = {
     "page": 1
 }
 
-for i in range(1, 5):
-    params["page"] = i
+
+# Data store info
+path = "/Users/shubh/Desktop/Ruby/R & D/ruby_reasearch/risk_arbitrage/data_store/newsapi_store.json"
+merger_headlines = {}
+with open(path) as f:
+  merger_headlines = json.load(f)
+
+
+# Func body
+more = True
+count = params["page"]
+while more:
+    params["page"] = count
     request = requests.get(url=api_endpoint, params=params)
     data = request.json()
-    for obj in data["articles"]:
-        print(obj["title"])
-        print()
+    try:
+        count += 1
+        for obj in data["articles"]:
+            if obj["title"] not in merger_headlines:
+                merger_headlines[obj["title"]] = {
+                    "source": obj["source"]["name"],
+                    "published_at": convertDates(obj["publishedAt"])
+                }
+    except Exception as e:
+        more = False
+        print(e)
 
-# with open(path, 'w') as outfile:
-#     json.dump(data, outfile)
+
+with open(path, 'w') as outfile:
+    json.dump(merger_headlines, outfile)
